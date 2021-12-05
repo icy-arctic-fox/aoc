0cbfd3f1111fd8032625b92d90cff56d7e8cc6c0
Michael Miller <icy.arctic.fox@gmail.com>
2021-12-04 22:47:10 -0700

    Day 5

---
+++ 2021/05/part-1.cr
@@ -0,0 +1,39 @@
+record Point, x : Int32, y : Int32
+record Line, a : Point, b : Point
+
+lines = [] of Line
+
+STDIN.each_line(chomp: true) do |line|
+  a, b = line.split(" -> ")
+  ax, ay = a.split(',').map(&.to_i)
+  bx, by = b.split(',').map(&.to_i)
+  lines << Line.new(
+    Point.new(ax, ay),
+    Point.new(bx, by)
+  )
+end
+
+width = lines.max_of { |line| Math.max(line.a.x, line.b.x) } + 1
+height = lines.max_of { |line| Math.max(line.a.y, line.b.y) } + 1
+
+grid = Array.new(width * height, 0)
+
+private macro index(x, y)
+  {{y.id}} * width + {{x.id}}
+end
+
+lines.each do |line|
+  if line.a.x == line.b.x
+    x = line.a.x
+    line.a.y.to(line.b.y) do |y|
+      grid[index(x, y)] += 1
+    end
+  elsif line.a.y == line.b.y
+    y = line.a.y
+    line.a.x.to(line.b.x) do |x|
+      grid[index(x, y)] += 1
+    end
+  end
+end
+
+puts grid.count(&.>(1))
