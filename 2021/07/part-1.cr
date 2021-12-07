aabf8b23416035711c5e77045bc5b2657153c832
Michael Miller <icy.arctic.fox@gmail.com>
2021-12-06 22:47:08 -0700

    Day 7

---
+++ 2021/07/part-1.cr
@@ -0,0 +1,9 @@
+positions = STDIN.gets(chomp: true).try(&.split(',').map(&.to_i))
+raise "Missing positions" unless positions
+
+min = positions.min
+max = positions.max
+
+puts (min..max).min_of do |target|
+  positions.sum { |position| (position - target).abs }
+end
---
+++ 2021/07/part-2.cr
@@ -0,0 +1,13 @@
+positions = STDIN.gets(chomp: true).try(&.split(',').map(&.to_i))
+raise "Missing positions" unless positions
+
+min = positions.min
+max = positions.max
+
+def gauss(x)
+  (x * (x + 1)) // 2
+end
+
+puts (min..max).min_of do |target|
+  positions.sum { |position| gauss((position - target).abs) }
+end
