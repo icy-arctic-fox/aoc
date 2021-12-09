a493bea9c43a5cfd86f99a26c62e549a43a986e7
Michael Miller <icy.arctic.fox@gmail.com>
2021-12-09 00:30:15 -0700

    Day 9

---
+++ 2021/09/part-1.cr
@@ -0,0 +1,71 @@
+require "bit_array"
+
+private def lower_neighbor?(x, y, w, h, map)
+  v = map[y][x]
+  case {x, y}
+  when {0, 0} # Top-left
+    map[0][1] <= v || map[1][0] <= v
+  when {0, h - 1} # Bottom-left
+    map[h - 2][0] <= v || map[h - 1][1] <= v
+  when {w - 1, 0} # Top-right
+    map[0][w - 2] <= v || map[1][w - 1] <= v
+  when {w - 1, h - 1} # Bottom-right
+    map[h - 1][w - 2] <= v || map[h - 2][w - 1] <= v
+  when {_, 0} # Top
+    map[0][x - 1] <= v || map[0][x + 1] <= v || map[1][x] <= v
+  when {0, _} # Left
+    map[y - 1][0] <= v || map[y + 1][0] <= v || map[y][1] <= v
+  when {w - 1, _} # Right
+    map[y - 1][w - 1] <= v || map[y + 1][w - 1] <= v || map[y][w - 2] <= v
+  when {_, h - 1} # Bottom
+    map[h - 1][x - 1] <= v || map[h - 1][x + 1] <= v || map[h - 2][x] <= v
+  else
+    map[y - 1][x] <= v || map[y + 1][x] <= v || map[y][x - 1] <= v || map[y][x + 1] <= v
+  end
+end
+
+map = Array(Array(Int32)).new
+
+STDIN.each_line(chomp: true) do |line|
+  map << line.chars.map(&.to_i)
+end
+
+w = map.first.size
+h = map.size
+remaining = BitArray.new(w * h, true)
+
+change = true
+while change
+  change = false
+  remaining.each_with_index do |flag, index|
+    next unless flag
+
+    y, x = index.divmod(w)
+    if lower_neighbor?(x, y, w, h, map)
+      remaining[index] = false
+      change = true
+    end
+  end
+end
+
+# remaining.each_with_index do |flag, index|
+#   y, x = index.divmod(w)
+#   v = map[y][x]
+#   puts if x.zero?
+#   if flag
+#     print("*#{v}")
+#   else
+#     print(" #{v}")
+#   end
+# end
+# puts
+
+values = [] of Int32
+remaining.each_with_index do |flag, index|
+  next unless flag
+
+  y, x = index.divmod(w)
+  values << map[y][x]
+end
+
+puts values.sum + values.size
---
+++ 2021/09/part-2.cr
@@ -0,0 +1,89 @@
+require "bit_array"
+
+private def neighbors(x, y, w, h)
+  case {x, y}
+  when {0, 0}         then [{1, 0}, {0, 1}]                             # Top-left
+  when {0, h - 1}     then [{0, h - 2}, {1, h - 1}]                     # Bottom-left
+  when {w - 1, 0}     then [{w - 2, 0}, {w - 1, 1}]                     # Top-right
+  when {w - 1, h - 1} then [{w - 2, h - 1}, {w - 1, h - 2}]             # Bottom-right
+  when {_, 0}         then [{x - 1, 0}, {x + 1, 0}, {x, 1}]             # Top
+  when {0, _}         then [{0, y - 1}, {0, y + 1}, {1, y}]             # Left
+  when {w - 1, _}     then [{w - 1, y - 1}, {w - 1, y + 1}, {w - 2, y}] # Right
+  when {_, h - 1}     then [{x - 1, h - 1}, {x + 1, h - 1}, {x, h - 2}] # Bottom
+  else                     [{x, y - 1}, {x, y + 1}, {x - 1, y}, {x + 1, y}]
+  end
+end
+
+private def lower_neighbor?(x, y, w, h, map)
+  v = map[y][x]
+  neighbors(x, y, w, h).any? { |(x2, y2)| map[y2][x2] <= v }
+end
+
+private def sink(map)
+  w = map.first.size
+  h = map.size
+
+  filter = BitArray.new(w * h, true)
+
+  changed = true
+  while changed
+    changed = false
+    filter.each_with_index do |flag, index|
+      next unless flag
+
+      y, x = index.divmod(w)
+      if yield(x, y, w, h)
+        filter[index] = false
+        changed = true
+      end
+    end
+  end
+
+  filter
+end
+
+private def expand(seed, map)
+  w = map.first.size
+  h = map.size
+
+  points = Set(Tuple(Int32, Int32)).new
+  points.add(seed)
+
+  changed = true
+  while changed
+    changed = false
+
+    new_points = points.flat_map do |(x, y)|
+      v = map[y][x]
+      neighbors(x, y, w, h).select do |(x2, y2)|
+        u = map[y2][x2]
+        u < 9 && u > v
+      end
+    end.to_set
+
+    merged = new_points | points
+    changed = merged != points
+    points = merged
+  end
+
+  points
+end
+
+map = Array(Array(Int32)).new
+
+STDIN.each_line(chomp: true) do |line|
+  map << line.chars.map(&.to_i)
+end
+
+low_points = sink(map) { |x, y, w, h| lower_neighbor?(x, y, w, h, map) }
+w = map.first.size
+
+points = low_points.each_with_index.compact_map do |flag, index|
+  next unless flag
+
+  y, x = index.divmod(w)
+  {x, y}
+end
+
+basins = points.map { |point| expand(point, map) }
+puts basins.map(&.size).to_a.sort!.last(3).product
