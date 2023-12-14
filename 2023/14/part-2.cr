6a864988824398a45d0b46eeaf134870279d7a1a
Michael Miller <icy.arctic.fox@gmail.com>
2023-12-13 23:42:55 -0700

    Loop detection

--- 2023/14/part-2.cr
+++ 2023/14/part-2.cr
@@ -1,5 +1,7 @@
 #!/usr/bin/env crystal
 
+require "bit_array"
+
 enum Cell : UInt8
   Empty
   Round
@@ -70,49 +72,51 @@ class Grid
   end
 
   def cycle
-    tilt_up
-    tilt_down
-    tilt_left
-    tilt_right
+    tilt_north
+    tilt_west
+    tilt_south
+    tilt_east
   end
 
-  def tilt_up : self
+  def tilt_north : self
     height.times do |y|
       width.times do |x|
-        move_up(x, y) if self[x, y].round?
+        move_north(x, y) if self[x, y].round?
       end
     end
     self
   end
 
-  def tilt_down : self
+  def tilt_south : self
     height.times do |y|
+      y = height - y - 1
       width.times do |x|
-        move_down(x, y) if self[x, y].round?
+        move_south(x, y) if self[x, y].round?
       end
     end
     self
   end
 
-  def tilt_left : self
+  def tilt_west : self
     height.times do |y|
       width.times do |x|
-        move_left(x, y) if self[x, y].round?
+        move_west(x, y) if self[x, y].round?
       end
     end
     self
   end
 
-  def tilt_right : self
+  def tilt_east : self
     height.times do |y|
       width.times do |x|
-        move_right(x, y) if self[x, y].round?
+        x = width - x - 1
+        move_east(x, y) if self[x, y].round?
       end
     end
     self
   end
 
-  def move_up(x, y) : Nil
+  def move_north(x, y) : Nil
     return if y <= 0
     cell = self[x, y]
     y.downto(1) do |y2|
@@ -122,7 +126,7 @@ class Grid
     end
   end
 
-  def move_down(x, y) : Nil
+  def move_south(x, y) : Nil
     return if y >= height - 1
     cell = self[x, y]
     y.upto(height - 2) do |y2|
@@ -132,7 +136,7 @@ class Grid
     end
   end
 
-  def move_left(x, y) : Nil
+  def move_west(x, y) : Nil
     return if x <= 0
     cell = self[x, y]
     x.downto(1) do |x2|
@@ -142,8 +146,8 @@ class Grid
     end
   end
 
-  def move_right(x, y) : Nil
-    return if x >= height - 1
+  def move_east(x, y) : Nil
+    return if x >= width - 1
     cell = self[x, y]
     x.upto(width - 2) do |x2|
       break unless self[x2 + 1, y].empty?
@@ -151,14 +155,40 @@ class Grid
       self[x2, y] = :empty
     end
   end
+
+  def state
+    BitArray.new(width * height) do |i|
+      y, x = i.divmod(width)
+      self[x, y].round?
+    end
+  end
 end
 
+def find_loop(states)
+  return if states.size < 2
+  last = states.last
+  index = states.rindex(last, states.size - 2)
+  return unless index
+
+  size = states.size - index - 1
+  offset = states.size % size
+  {offset, size}
+end
+
+states = [] of UInt64
 grid = Grid.from_io(STDIN)
-1000000000.times do |i|
+
+CYCLES = 1000000000
+
+CYCLES.times do |i|
   grid.cycle
-  if i.divisible_by?(100000)
-    STDERR.puts grid
-    STDERR.puts "#{i / 1000000000 * 100}%"
+  states << grid.state.hash
+  if l = find_loop(states)
+    offset, size = l
+    loop_states = states[offset, size]
+    remaining = (CYCLES - offset) % size
+    remaining.times { grid.cycle }
+    break
   end
 end
 puts grid.load
